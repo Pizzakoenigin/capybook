be63262b4b1d743b860ee4e3e06f50002a83633b
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-05-16 12:01:54 +0200

    capy card text bild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CapyCardComponent } from './capy-card/capy-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CapyCardComponent],
+  imports: [RouterOutlet, HeaderComponent, CapyCardComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   hamsterTexts = [
+    'Mein Name ist Carlos und ich bin sehr müde',
+    'Ich bin Horst und fresse gerne Gras.',
     'Hallo, ich bin Frederik das Capybara.',
-    'Ich bin Carlos.',
-    'Ich fresse gerne Gras.',
     'Hey, bist du auch ein Capybara?'
   ]
 
---
+++ src/app/proposals/proposals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proposals',
+  standalone: true,
+  imports: [],
+  templateUrl: './proposals.component.html',
+  styleUrl: './proposals.component.scss'
+})
+export class ProposalsComponent {
+
+}
