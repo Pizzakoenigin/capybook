32bea2c1e4fa6775f3330c67dae132f27bfc7693
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-05-21 17:27:20 +0200

    mock-posts

---
+++ src/mock-posts.ts
@@ -0,0 +1,30 @@
+import { Post } from "./app/posts";
+
+export const POSTS: Post[] = [
+    {
+        id: 1,
+        author: 'Carlos',
+        content: 'Mein Name ist Carlos und ich bin sehr müde',
+        source: 'assets/img/capybara1.jpg',
+        likes: 0
+    },
+    {
+        id: 2,
+        author: 'Horst',
+        content: 'Ich bin Horst und fresse gerne Gras.',
+        source: 'assets/img/capybara2.jpg',
+        likes: 0
+    }, {
+        id: 3,
+        author: 'Frederik',
+        content: 'Hallo, ich bin Frederik das Capybara.',
+        source: 'assets/img/capybara3.jpg',
+        likes: 0
+    }, {
+        id: 4,
+        author: 'Matilde',
+        content: 'Hey, bist du auch ein Capybara?',
+        source: 'assets/img/capybara4.jpg',
+        likes: 0
+    }
+]
