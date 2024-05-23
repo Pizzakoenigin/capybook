13e48ad7b496a4ed99a1a1c7bfea08727458c4bc
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-05-23 13:48:39 +0200

    wip re arranging friendbox

--- src/app/profile-row/profile-row.component.ts
+++ src/app/profile-row/profile-row.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FriendService } from '../friend.service';
 import { NgIf } from '@angular/common';
+import { CAPYBARAS } from '../../mock-capybara';
 
 @Component({
   selector: 'app-profile-row',
@@ -10,10 +11,9 @@ import { NgIf } from '@angular/common';
   styleUrl: './profile-row.component.scss'
 })
 export class ProfileRowComponent {
-  @Input() name = 'Capybara';
-  @Input() source = 'assets/img/capybara12.jpg';
-  @Input() text = 'Das bist du';
-  @Input() canFollow = true;
+  // @Input() canFollow = true;
+
+  capybaras = CAPYBARAS
 
   constructor(public fs:FriendService) {
 
--- src/app/proposals/proposals.component.ts
+++ src/app/proposals/proposals.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { FriendService } from '../friend.service';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProfileRowComponent } from '../profile-row/profile-row.component';
 import { CAPYBARAS } from '../../mock-capybara';
@@ -7,10 +8,14 @@ import { CAPYBARAS } from '../../mock-capybara';
 @Component({
   selector: 'app-proposals',
   standalone: true,
-  imports: [RouterOutlet, ProfileRowComponent, NgFor],
+  imports: [RouterOutlet, ProfileRowComponent, NgFor, NgIf],
   templateUrl: './proposals.component.html',
   styleUrl: './proposals.component.scss'
 })
 export class ProposalsComponent {
   capybaras = CAPYBARAS
+
+  constructor(public fs:FriendService) {
+
+  }
 }
