41ac30b246b73063a65e7135df161b16b885d409
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-05-24 15:34:58 +0200

    link friendbox

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ProposalsComponent } from '../proposals/proposals.component';
+import { FriendboxComponent } from '../friendbox/friendbox.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [HeaderComponent, ProposalsComponent],
+  imports: [HeaderComponent, ProposalsComponent, FriendboxComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
