ff3cf35cdc9d8502e2940f92a07610810c72180c
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-05-24 16:31:37 +0200

    styling

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../header/header.component';
 import { ProposalsComponent } from '../proposals/proposals.component';
 import { FriendboxComponent } from '../friendbox/friendbox.component';
 import { CAPYBARAS } from '../../mock-capybara';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [HeaderComponent, ProposalsComponent, FriendboxComponent],
+  imports: [HeaderComponent, ProposalsComponent, FriendboxComponent, RouterOutlet, RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
