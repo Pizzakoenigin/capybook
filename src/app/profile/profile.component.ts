8e1a9efa14290484411ec4352d98380f68979145
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-05-24 17:57:56 +0200

    wip routes to individual profiles

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ProposalsComponent } from '../proposals/proposals.component';
 import { FriendboxComponent } from '../friendbox/friendbox.component';
 import { CAPYBARAS } from '../../mock-capybara';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -12,6 +12,20 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
   capybaras = CAPYBARAS
+
+  data: any;
+
+  constructor(private route: ActivatedRoute) {
+    
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.data = params['id']
+      console.log(this.data);
+      
+    })
+  }
 }
--- src/app/proposals/proposals.component.ts
+++ src/app/proposals/proposals.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { FriendService } from '../friend.service';
 import { NgFor, NgIf } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ProfileRowComponent } from '../profile-row/profile-row.component';
 import { CAPYBARAS } from '../../mock-capybara';
 
 @Component({
   selector: 'app-proposals',
   standalone: true,
-  imports: [RouterOutlet, ProfileRowComponent, NgFor, NgIf],
+  imports: [RouterOutlet, RouterLink, ProfileRowComponent, NgFor, NgIf],
   templateUrl: './proposals.component.html',
   styleUrl: './proposals.component.scss'
+
+  
 })
+
+
+
 export class ProposalsComponent {
+  
   capybaras = CAPYBARAS
 
   constructor(public fs:FriendService) {
