66983797441b56679cc3084620b7cbad1f114a63
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-05-24 14:41:25 +0200

    init navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,9 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { AppComponent } from './app.component';
+import { ProfilComponent } from './profil/profil.component';
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProfilComponent } from './profil/profil.component';
 
 export const routes: Routes = [];
