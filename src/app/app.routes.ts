db07e27d2aaf3aa77c83cc129296dcab81d72958
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-06-05 13:39:15 +0200

    added main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { MainComponent } from './main/main.component'
 import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
-    {path: 'main', component: MainComponent},
+    {path: '', component: MainComponent},
     {path: 'profile', component: ProfileComponent}
 ];
