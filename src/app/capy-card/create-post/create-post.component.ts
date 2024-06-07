ae572cf697f013b9db8c626dee098cea778ed71a
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-06-07 14:51:41 +0200

    buttons, styling

--- src/app/capy-card/create-post/create-post.component.ts
+++ src/app/capy-card/create-post/create-post.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-post',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.scss'
 })
 export class CreatePostComponent {
+  showInput: boolean = false
 
+  createInput() {
+    this.showInput = !this.showInput
+  }
 }
