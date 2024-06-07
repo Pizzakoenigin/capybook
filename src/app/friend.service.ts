1ccc97a675e895ec3d9e03f71ed9619d728d8ee7
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-06-07 12:26:06 +0200

    friend service working

--- src/app/friend.service.ts
+++ src/app/friend.service.ts
@@ -3,72 +3,47 @@ import { DataService } from "./data.service";
 import { on } from "events";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
 export class FriendService {
 
-    constructor(public dataService: DataService) {}
+  constructor(public dataService: DataService) { }
 
-addFriend(capybaras, i) {
+  addFriend(capybaras, i) {
     capybaras[0].friends.push(capybaras[i])
-    capybaras[i].canFollow=false;
-
-
-    
-
+    capybaras[i].canFollow = false;
 
     capybaras[i].friends.push(capybaras[0])
-    
-    
-    this.dataService.friendListLength = capybaras[0].friends.length+2
+
+    this.dataService.friendListLength = capybaras[0].friends.length + 2
   }
 
   removeFriend(capybaras, i) {
-    capybaras[0].friends.splice(i, 1)
+    for (let k = 0; k < capybaras[0].friends.length; k++) {
+      if (capybaras[0].friends[k].id == i) {
+        capybaras[0].friends.splice(k, 1)
+      }
 
-    console.log(i);
-    
-    console.log(capybaras[i].friends);
-    
-    // console.log(capybaras[this.dataService.profileIndex].id); //0 because the user is always me
-
+    }
 
 
 
-    // get id from the capy i clicked on
+    // remove user(me) from the other capybaras friend list
+
+    // get id from the capy i clicked on 
 
     // remove capy with the id=0 (me) from the capy's friendlist
 
 
+    for (let j = 0; j < capybaras[i].friends.length; j++) {
+      if (capybaras[i].friends[j].id == 0) {
+        capybaras[i].friends.splice(j, 1)
+      }
+    }
 
 
-    // capybara[i].friends.splice
-
-
-
-    // capybaras[i].friends = capybaras[i].friends.filter(friend => friend.capybaras.id !== 0);
-
-
-     
-
-    // capybaras[i].friends = capybaras[i].friends.filter(capybaras[i].friends.capybaras.id !== 0)
-    
-    // capybaras[i].friends = capybaras[i].friends.filter(friend => friend !== capybaras[0])
-
-
-    // const findOwnCapybaraIndex = capybaras[i].friends.findIndex(friend => friend === capybaras[0]);
-
-    // capybaras[i].friends.splice(findOwnCapybaraIndex, 1)
-
-    // capybaras[i].friends.splice(capybaras[i].friends.indexOf(capybaras[0]), 1);
-
-    
-
-
-    // capybaras[i].friends = capybaras[i].friends.filter(friend => friend !== capybaras[0]);
-
-    this.dataService.friendListLength = capybaras[0].friends.length+2
+    this.dataService.friendListLength = capybaras[0].friends.length + 2
   }
 
 }
