635bf991da2e3ccd470e9c0e76760a8c74c0b730
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-06-07 10:43:35 +0200

    styling

--- src/app/friend.service.ts
+++ src/app/friend.service.ts
@@ -12,12 +12,20 @@ export class FriendService {
 addFriend(capybaras, i) {
     capybaras[0].friends.push(capybaras[i])
     capybaras[i].canFollow=false;
+
+
     // capybaras[i].friends.push(capybaras[0])
+    
+    
     this.dataService.friendListLength = capybaras[0].friends.length+2
   }
 
   removeFriend(capybaras, i) {
     capybaras[0].friends.splice(i, 1)
+
+    // capybaras[i].friends.splice(capybaras[i].friends.indexOf(capybaras[0]), 1);
+
+
     this.dataService.friendListLength = capybaras[0].friends.length+2
   }
 
