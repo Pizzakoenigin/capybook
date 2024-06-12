dab0b7607f247cf92cb7555be05c1ccf30c6ad2c
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-06-12 17:36:05 +0200

    routerLink works

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,14 @@
 
 import { Component } from '@angular/core';
 import { ProfileChildComponent } from './profile-child/profile-child.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-profile',
     standalone: true,
     imports: [
-        ProfileChildComponent
+        ProfileChildComponent,
+        RouterOutlet
     ],
     templateUrl: './profile.component.html',
     styleUrl: './profile.component.scss'
