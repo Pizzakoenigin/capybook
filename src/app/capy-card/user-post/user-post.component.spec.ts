c5c2c90d4a80bfa4ecb0b35507f1b14cef77b575
FranziskaRoder <Franziska.roder@curvedesign.de>
2024-06-14 10:33:32 +0200

    wip

---
+++ src/app/capy-card/user-post/user-post.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserPostComponent } from './user-post.component';
+
+describe('UserPostComponent', () => {
+  let component: UserPostComponent;
+  let fixture: ComponentFixture<UserPostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserPostComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(UserPostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/capy-card/user-post/user-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-post.component.html',
+  styleUrl: './user-post.component.scss'
+})
+export class UserPostComponent {
+
+}
